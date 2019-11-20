032c303d4b17c183dc2fcd15ce9c24ad31c30736
Alann Maulana <alann_it06@yahoo.co.id>
2019-11-20 15:52:57 +0700

    remove AppTest, add beacon.types test

---
+++ src/beacon.types.test.ts
@@ -0,0 +1,57 @@
+import {
+    AuthorizationStatus,
+    ManagerState,
+    Region,
+    AuthorizationStatusCallback,
+    RangingDidRangeBeaconsCallback,
+    MonitoringDidEnterRegionCallback,
+    MonitoringDidExitRegionCallback,
+    MonitoringDidDetermineStateCallback,
+} from './beacon.types';
+
+test('all region parameter value is undefined or null', () => {
+    const region: Region = {
+        identifier: null,
+        uuid: null
+    };
+    expect(region.identifier).toBe(null);
+    expect(region.uuid).toBe(null);
+    expect(region.major).toBe(undefined);
+    expect(region.minor).toBe(undefined);
+});
+
+test('identifier and uuid are equals', () => {
+    const region: Region = {
+        identifier: 'Cubeacon',
+        uuid: 'CB10023F-A318-3394-4199-A8730C7C1AEC'
+    };
+    expect(region.identifier).toBe('Cubeacon');
+    expect(region.uuid).toBe('CB10023F-A318-3394-4199-A8730C7C1AEC');
+    expect(region.major).toBe(undefined);
+    expect(region.minor).toBe(undefined);
+});
+
+test('major is not null', () => {
+    const region: Region = {
+        identifier: 'Cubeacon',
+        uuid: 'CB10023F-A318-3394-4199-A8730C7C1AEC',
+        major: 123
+    };
+    expect(region.identifier).toBe('Cubeacon');
+    expect(region.uuid).toBe('CB10023F-A318-3394-4199-A8730C7C1AEC');
+    expect(region.major).toEqual(123);
+    expect(region.minor).toEqual(undefined);
+});
+
+test('major is not null', () => {
+    const region: Region = {
+        identifier: 'Cubeacon',
+        uuid: 'CB10023F-A318-3394-4199-A8730C7C1AEC',
+        major: 123,
+        minor: 456
+    };
+    expect(region.identifier).toBe('Cubeacon');
+    expect(region.uuid).toBe('CB10023F-A318-3394-4199-A8730C7C1AEC');
+    expect(region.major).toEqual(123);
+    expect(region.minor).toEqual(456);
+});
