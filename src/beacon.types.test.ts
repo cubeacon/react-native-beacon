1f2a240ad79b3e4b561584cd0dda2a9ccc4f0277
Alann Maulana <alann_it06@yahoo.co.id>
2019-12-02 16:38:05 +0700

    Update beacon.types.test.ts

--- src/beacon.types.test.ts
+++ src/beacon.types.test.ts
@@ -3,11 +3,9 @@ import {
     AuthorizationStatus,
     BluetoothState,
     Region,
-    AuthorizationStatusCallback,
-    DidRangeBeaconsCallback,
-    DidEnterRegionCallback,
-    DidExitRegionCallback,
-    DidDetermineStateCallback,
+    Proximity,
+    Beacon,
+    RegionState,
 } from './beacon.types';
 /* eslint-enable */
 
@@ -56,4 +54,84 @@ test('major is not null', () => {
     expect(region.uuid).toBe('CB10023F-A318-3394-4199-A8730C7C1AEC');
     expect(region.major).toEqual(123);
     expect(region.minor).toEqual(456);
+});
+
+test('all beacon value from iOS is set', () => {
+    const beacon: Beacon = {
+        uuid: 'CB10023F-A318-3394-4199-A8730C7C1AEC',
+        major: 123,
+        minor: 456,
+        accuracy: 1.2,
+        proximity: "immediate",
+        rssi: -57
+    };
+    expect(beacon.uuid).toBe('CB10023F-A318-3394-4199-A8730C7C1AEC');
+    expect(beacon.major).toEqual(123);
+    expect(beacon.minor).toEqual(456);
+    expect(beacon.accuracy).toEqual(1.2);
+    expect(beacon.proximity).toEqual("immediate");
+    expect(beacon.rssi).toEqual(-57);
+});
+
+test('all beacon value from Android is set', () => {
+    const beacon: Beacon = {
+        uuid: 'CB10023F-A318-3394-4199-A8730C7C1AEC',
+        major: 456,
+        minor: 789,
+        accuracy: 2.3,
+        proximity: "near",
+        rssi: -59,
+        macAddress: "00:11:22:33:44:55",
+        txPower: -58
+    };
+    expect(beacon.uuid).toBe('CB10023F-A318-3394-4199-A8730C7C1AEC');
+    expect(beacon.major).toEqual(456);
+    expect(beacon.minor).toEqual(789);
+    expect(beacon.accuracy).toEqual(2.3);
+    expect(beacon.proximity).toEqual("near");
+    expect(beacon.rssi).toEqual(-59);
+    expect(beacon.macAddress).toEqual("00:11:22:33:44:55");
+    expect(beacon.txPower).toEqual(-58);
+});
+
+test('authorization status must be undefined', () => {
+    var status: AuthorizationStatus;
+    expect(status).toBe(undefined);
+});
+
+test('authorization status must be "authorizedAlways" and "notDetermined"', () => {
+    var status: AuthorizationStatus = "authorizedAlways";
+    expect(status).toBe("authorizedAlways");
+    status = "notDetermined";
+    expect(status).toBe("notDetermined");
+});
+
+test('bluetooth state must be undefined', () => {
+    var status: BluetoothState;
+    expect(status).toBe(undefined);
+});
+
+test('bluetooth state must be "powerOn"', () => {
+    var status: BluetoothState = "powerOn";
+    expect(status).toBe("powerOn");
+});
+
+test('proximity must be undefined', () => {
+    var proximity: Proximity;
+    expect(proximity).toBe(undefined);
+});
+
+test('proximity must be "immediate"', () => {
+    var proximity: Proximity = "immediate";
+    expect(proximity).toBe("immediate");
+});
+
+test('region state must be undefined', () => {
+    var state: RegionState;
+    expect(state).toBe(undefined);
+});
+
+test('region state must be "inside"', () => {
+    var state: RegionState = "inside";
+    expect(state).toBe("inside");
 });
